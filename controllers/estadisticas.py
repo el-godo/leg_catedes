4b64bc275002579e96d3b2eb53e444e2e476f610
el-godo <archy_22_2@hotmail.com>
2020-06-22 13:25:06 -0300

    sacando totalidades en el modulo de estadisticas

--- controllers/estadisticas.py
+++ controllers/estadisticas.py
@@ -3,6 +3,40 @@
 def menu():
 
 	return dict()
+def totales():
+	from datetime import datetime, date, time, timedelta
+	import calendar
+	fecha=datetime.now()
+	hoy_anio=fecha.year
+	hoy_mes=fecha.month
+	set_leg=db(db.legajo.estado).select()
+	ctp=0   #contador total de escuela de cadetes-->
+	p1=0	#contador de total de cadetes de policia de 1 año
+	p2=0 	#contador de total de cadetes de policia de 2 año
+	p3=0 	#contador de total de cadetes de policia de 3 año
+	p1b=0	#contador de total de cadetes de policia de 1 año de baja
+	p2b=0	#contador de total de cadetes de policia de 2 año de baja
+	p3b=0	#contador de total de cadetes de policia de 3 año de baja
+	cts=0 	#contador total de escuela sevicio penitenciario-->
+	s1=0	#contador de total de cadetes del servicio de 1 año
+	s2=0	#contador de total de cadetes del servicio de 2 año
+	s3=0	#contador de total de cadetes del servicio de 3 año
+	s1b=0	#contador de total de cadetes del servicio de 1 año de baja
+	s2b=0	#contador de total de cadetes del servicio de 2 año de baja
+	s3b=0	#contador de total de cadetes del servicio de 3 año de baja
+	atie=0   # Acumulador total de inscriptos en el establecimiento
+	ta=0
+
+
+ 
+
+
+
+
+
+
+	return dict(set_leg=set_leg,ctp=ctp,p1=p1,p2=p2,p3=p3,p1b=p1b,p2b=p2b,p3b=p3b,s1b=s1b,s2b=s2b,s3b=s3b,
+		s1=s1,s2=s2,s3=s3,atie=atie,ta=ta)	
 
 def sin_autorizacion():
 	return dict()
@@ -25,6 +59,9 @@ def cant_sanciones():
 	csem=0 # compara sancion mas alta del mes
 	idma=0 # id de mayor dias de arresyto del año
 	idmm=0 # id de mayor dias de arresyto del mes
+	
+
+
 	return dict(set_sanciones=set_sanciones,hoy_anio=hoy_anio,hoy_mes=hoy_mes,csa=csa,csm=csm,)
 
 def cant_n_medicas():
